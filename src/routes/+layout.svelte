cb7e6b8cbc374662ff563dd048c1f064dc7770af
Jiong Yan Yap <jiongyan@usc.edu>
2022-12-16 13:34:42 -0800

    Set up sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  import "chota";
+  import "../app.css";
+  import { onMount } from "svelte";
+
+  // Set dark mode if it is preferred
+  onMount(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      document.body.classList.add("dark");
+    }
+  });
+</script>
+
+<nav class="nav">
+  <div class="nav-center">
+    <div class="tabs">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+  <slot />
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<h1>About</h1>
+
+<p>
+  Hi! Just a passerby who's way too into vocaloid, games, and programming. In no
+  particular order.
+</p>
+
+<p>
+  Font used: <a href="https://int10h.org/oldschool-pc-fonts/">IBM VGA 8x16</a> by
+  VileR
+</p>
